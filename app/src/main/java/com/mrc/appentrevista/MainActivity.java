cb0fcc6e4689abde2c33728d413f2c6c132b46d3
mauricio.carvalho <mauricio.cac@hotmail.com>
2020-04-25 20:11:49 -0300

    Desenhando a tela de Detalhes do cliente

--- app/src/main/java/com/mrc/appentrevista/MainActivity.java
+++ app/src/main/java/com/mrc/appentrevista/MainActivity.java
@@ -2,13 +2,27 @@ package com.mrc.appentrevista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnEntrar = findViewById(R.id.btnentrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication(), InformacoesActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
